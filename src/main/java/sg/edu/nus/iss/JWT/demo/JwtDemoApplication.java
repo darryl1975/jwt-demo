08efa7780415a9e5ce718ee84c95efc9b3c4a330
Darryl Ng <darryl1975@hotmail.com>
2023-03-03 13:03:46 +0800

    added references

--- src/main/java/sg/edu/nus/iss/JWT/demo/JwtDemoApplication.java
+++ src/main/java/sg/edu/nus/iss/JWT/demo/JwtDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // References
 // https://github.com/Ozair0/Spring-Boot-3-Auth-JWT-Cookie-JPA/tree/master/src/main/java/com/example/security/full/security/UserSecurity
-
+// https://github.com/ali-bouali/spring-boot-3-jwt-security/blob/main/src/main/java/com/alibou/security/
 
 @SpringBootApplication
 public class JwtDemoApplication {
